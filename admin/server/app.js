d2d13d9528bfcd3f392b67125d41319edf97633c
haniyshaban <32319595+haniyshaban@users.noreply.github.com>
2026-02-21 04:04:36 +0530

    fix: add PATCH to CORS allowed methods

--- admin/server/app.js
+++ admin/server/app.js
@@ -12,7 +12,7 @@ app.use(helmet());
 app.use(cors({
   origin: true, // allow all origins
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.options('*', cors());
